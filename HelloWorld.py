847be0105523be8e702f72723b41a4d4118e187c
Darsh D. Coding <darshdcoding@gmail.com>
2026-08-13 22:50:25 +0530

    strating again

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+num1= int(input("Enter First Number: "))
+num2= int(input("Enter Second Number: "))
+
+try:
+    print (int(num1/num2))
+except Exception as e:
+    print(f"Chutiya hai kya. Error: {e}")
+
+
+try:
+    ajfa;dfj
+except Exception as e:
+    print (e)
+
